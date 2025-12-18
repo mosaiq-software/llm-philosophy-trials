332d56b06b87ea7753c23ace8acda4b4ceeb02a2
Tran Minh Duc Nguyen <tnguyen6@wpi.edu>
2025-12-18 18:52:59 -0500

    dashboard update

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-//resize
 const divider = document.querySelector(".divider");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
@@ -25,4 +23,33 @@ document.addEventListener("mousemove", (e) => {
 
 document.addEventListener("mouseup", () => {
     isResizing = false;
+});
+
+const infoBtn = document.querySelector("#infoBtn");
+const infoModal = document.querySelector("#infoModal");
+const closeModal = document.querySelector("#closeModal");
+
+if (infoBtn && infoModal && closeModal) {
+    infoBtn.addEventListener("click", () => {
+        infoModal.classList.add("show");
+    });
+
+    closeModal.addEventListener("click", () => {
+        infoModal.classList.remove("show");
+    });
+
+    infoModal.addEventListener("click", (e) => {
+        if (e.target === infoModal) {
+            infoModal.classList.remove("show");
+        }
+    });
+}
+
+const modelTabs = document.querySelectorAll(".model-tab");
+
+modelTabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        modelTabs.forEach((t) => t.classList.remove("active"));
+        tab.classList.add("active");
+    });
 });
